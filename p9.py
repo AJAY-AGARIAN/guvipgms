c0756dc5198e6ab643eb14d15f12d52959d6a2d3
AJAY-AGARIAN <38047216+AJAY-AGARIAN@users.noreply.github.com>
2019-07-06 14:07:55 +0530

    Update p9.py

--- p9.py
+++ p9.py
@@ -1,8 +1,11 @@
 h=int(input())
 j=int(input())
-for i in range(h,j+1):
-    for f in range(2,i):
-        if(i%f==0):
-            break
+count=[]
+for i in range(h+1,j+1):
+    if(i>1):
+        for f in range(2,i):
+            if(i%f==0):
+                break
         else:
-            print(i)
+            count.append(f)
+print(len(count)+1)
