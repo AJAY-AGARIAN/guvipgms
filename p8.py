e9af4cf3f6399c544504e3fec442477cad239fd2
AJAY-AGARIAN <38047216+AJAY-AGARIAN@users.noreply.github.com>
2019-07-06 12:05:03 +0530

    Update p8.py

--- p8.py
+++ p8.py
@@ -3,5 +3,6 @@ if(j[0].islower):
     j[0]=j[0].upper()
 for k in range(0,len(j)):
     if(j[k]==' '):
-        j[k+1]=j[k+1].upper()
+        if(j[k+1].islower):
+            j[k+1]=j[k+1].upper()
 print(''.join(j))
