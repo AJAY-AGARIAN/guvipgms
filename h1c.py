eb158c6d61cb2a3fb88040a67483189760f515a9
AJAY-AGARIAN <38047216+AJAY-AGARIAN@users.noreply.github.com>
2019-07-06 18:44:29 +0530

    Update and rename h1.py to h1c.py

--- h1c.py
+++ h1c.py
@@ -1,5 +1,7 @@
+import numpy as np
 k=int(input())
 i=[]
+no=0
 for j in range(0,k):
     i.append(input())
 p=[]
@@ -7,11 +9,18 @@ for j in range(0,len(i)):
     for k in range(j+1,len(i)):
         if(i[j]==i[k]):
             p.append(i[j])
-print(p)
+            no=no+1
+if(no==0):
+    print('unique')
+elif p:
+    unique(p)
+def unique(list1): 
+    x = np.array(list1) 
+    tm=np.unique(x)
+    print(''.join(tm))
 for c in range(0,len(p)):
     for g in range(c+1,len(p)):
         if(p[c]>p[g]):
             temp=p[g]
             p[g]=p[c]
             p[c]=temp
-print(''.join(p))
